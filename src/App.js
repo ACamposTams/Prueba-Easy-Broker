aa54fd51c000fce96eb9113f2a999e6eaf990f49
Alfredo Campos Tams <a01020939@itesm.mx>
2018-08-30 11:20:58 -0500

    Cliente agregado
    Se agregó cliente axios para el consumo del endpoint, se imprime por el
    momento la respuesta

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  
+  componentDidMount() {
+    axios.get('http://www.mocky.io/v2/5b6a3202320000ed11af5f20')
+      .then(
+        function(response) {
+          console.log(response.data)
+        }
+      )
+  }
+  
   render() {
     return (
       <div className="App">
